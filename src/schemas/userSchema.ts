2c7d21b561857b47cd6ff5d92ac03725eec88b03
MariaDuda <mariabevenuto062@gmail.com>
2022-09-13 12:57:34 -0300

    [FIX] user register schema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -4,7 +4,7 @@ import { IuserData } from "../types/userTypes";
 export const userRegisterSchema = joi.object<IuserData>({
     email: joi.string().email().required(),
     password: joi.string().required(),
-    passwordRef: joi.string().required()
+    passwordRef: joi.string().valid(joi.ref("password")).required()
 })
 
 export const userLoginSchema = joi.object<IuserData>({

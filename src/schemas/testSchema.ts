5a8897d2a5e910a6ff7351e3a4ffa9938ecc9d83
MariaDuda <mariabevenuto062@gmail.com>
2022-09-13 16:48:32 -0300

    [ADD] type to schema test

--- src/schemas/testSchema.ts
+++ src/schemas/testSchema.ts
@@ -1,6 +1,7 @@
 import joi from "joi";
+import {ItestInput} from "../types/testTypes"
 
-export const testSchema = joi.object({
+export const testSchema = joi.object<ItestInput>({
     name: joi.string().required(),
     pdfUrl: joi.string().uri().required(),
     discipline: joi.string().required(),

9784b8ccfea1303b397b9653ee67cc1887b76373
MariaDuda <mariabevenuto062@gmail.com>
2022-09-13 14:26:55 -0300

    fix spaces

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -6,5 +6,6 @@ import { ItestInput } from "../types/testTypes";
 export async function createTest(req:Request,res:Response) {
     const test : ItestInput = req.body;
     await testService.createTest(test);
+    
     res.sendStatus(httpsUtils.CREATED);
 }

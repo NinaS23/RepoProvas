48bd3ea4b051576e1831e4744a646182328838df
MariaDuda <mariabevenuto062@gmail.com>
2022-09-16 13:28:23 -0300

    [FEAT] tests for headers

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -24,6 +24,36 @@ describe("test route POST /test", () => {
         
     });
 
+    it("send a test with the incorrect schema", async () => {
+        const user = factory.loginUser(2);
+        const result = await supertest(app).post("/sign-in").send(user);
+        expect(result.statusCode).toBe(200);
+        const token = result.body.token;
+        expect(token).not.toBeNull();
+       
+
+        const testData = testFactory.createFakerTestData(true);
+        const testResult = await supertest(app).post("/test").send(testData).set("Authorization", `Bearer ${token}`);
+        expect(testResult.statusCode).toBe(404);
+        
+    });
+
+
+    it("send a test without the Headers", async () => {
+        const user = factory.loginUser(2);
+        const result = await supertest(app).post("/sign-in").send(user);
+        expect(result.statusCode).toBe(200);
+        const token = result.body.token;
+        expect(token).not.toBeNull();
+       
+
+        const testData = testFactory.createFakerTestData(false);
+        const testResult = await supertest(app).post("/test").send(testData);
+        expect(testResult.statusCode).toBe(401);
+        
+    });
+
+
    
 
 });

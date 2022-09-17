8bb6ed3c6d2e5570d5f57b9561faa43107e39b39
MariaDuda <mariabevenuto062@gmail.com>
2022-09-17 18:49:35 -0300

    [FEAT] dotenv in comment

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from "./app/app";
-
+/* import dotenv from "dotenv";
+ */
 app.listen(process.env.PORT, () => {
     console.log(`Server is up and running on port ${process.env.PORT}`)
 });
